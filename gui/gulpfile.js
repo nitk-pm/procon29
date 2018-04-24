32dd4fc468042191cb2b868d8070308d1b43e6e7
namachan10777 <namachan10777@gmail.com>
2018-04-24 20:03:01 +0900

    elm setting for gulp

--- gui/gulpfile.js
+++ gui/gulpfile.js
@@ -2,10 +2,24 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var concat = require('gulp-concat');
 var gulp_typings = require('gulp-typings');
+var gulp_elm = require('gulp-elm');
 
-gulp.task('build', () => {
+gulp.task('elm-init', gulp_elm.init);
+
+gulp.task('elm', ['elm-init'], () =>
+	gulp.src('src/*.elm')
+		.pipe(gulp_elm())
+		.pipe(gulp.dest('./app'))
+);
+
+gulp.task('html', () =>
+	gulp.src(['./src/index.html'])
+		.pipe(gulp.dest('./app'))
+);
+
+gulp.task('typescript', () => {
 	var proj = ts.createProject('./tsconfig.json');
-	gulp.src([
+	return gulp.src([
 		'./typings/index.d.ts',
 		'./src/**/*.ts',
 		'!./node_modules/**',
@@ -14,9 +28,6 @@ gulp.task('build', () => {
 	.js
 	.pipe(concat('index.js'))
 	.pipe(gulp.dest('./app'));
-
-	gulp.src([
-		'./src/index.html'
-	])
-	.pipe(gulp.dest('./app'));
 });
+
+gulp.task('default', ['elm', 'typescript', 'html']);
