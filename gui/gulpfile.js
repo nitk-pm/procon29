eb4abb6a94af4d5cb6ed2ba9416bbc315284d7cc
namachan10777 <namachan10777@gmail.com>
2018-04-28 02:12:10 +0900

    remove elm 正気を取り戻した

--- gui/gulpfile.js
+++ gui/gulpfile.js
@@ -2,18 +2,14 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var concat = require('gulp-concat');
 var gulp_typings = require('gulp-typings');
-var gulp_elm = require('gulp-elm');
-
-gulp.task('elm-init', gulp_elm.init);
-
-gulp.task('elm', ['elm-init'], () =>
-	gulp.src('src/*.elm')
-		.pipe(gulp_elm())
-		.pipe(gulp.dest('./app'))
-);
 
 gulp.task('html', () =>
 	gulp.src(['./src/index.html'])
+		.pipe(gulp.dest('./app'))
+);
+
+gulp.task('package.json', () =>
+	gulp.src(['./package.json'])
 		.pipe(gulp.dest('./app'))
 );
 
@@ -26,7 +22,7 @@ gulp.task('typescript', () => {
 	])
 	.pipe(proj())
 	.js
-	.pipe(concat('index.js'))
+	.pipe(concat('main.js'))
 	.pipe(gulp.dest('./app'));
 });
 
@@ -38,10 +34,11 @@ gulp.task('watch-html', () =>
 	gulp.watch('./src/*.html', ['html'])
 );
 
-gulp.task('watch-elm', () =>
-	gulp.watch('./src/*.elm', ['elm'])
+
+gulp.task('watch-html', () =>
+	gulp.watch('./package.json', ['package.json'])
 );
 
-gulp.task('watch', ['watch-ts', 'watch-html', 'watch-elm']);
+gulp.task('watch', ['watch-ts', 'watch-html', 'watch-package.json']);
 
-gulp.task('default', ['elm', 'typescript', 'html']);
+gulp.task('default', ['typescript', 'html', 'package.json']);
