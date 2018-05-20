6d7315236728bd18c73d2b3f3844c096c462daef
namachan10777 <namachan10777@gmail.com>
2018-05-21 00:58:52 +0900

    modules/historyを消し

--- gui/src/renderer/actions.ts
+++ gui/src/renderer/actions.ts
@@ -1,13 +1,11 @@
 import * as BoardModule from './modules/board';
-import * as HistModule from './modules/history';
 import * as DrawerModule from './modules/drawer';
 
 export enum Names {
 	CLICK_END_TURN = 'IGOKABADDI_CLICK_END_TURN',
 	CLICK_SQUARE  = 'IGOKABADDI_CLICK_SQUARE',
-	STACK_HISTORY = 'IGOKABADDI_STACK_HISTORY',
 	CLOSE = 'IGOKABADDI_CLOSE',
 	TOGGLE_DRAWER = 'IGOKABADDI_TOGGLE_DRAWER'
 }
 
-export type T = BoardModule.ClickSquareAction | HistModule.StackHistoryAction | DrawerModule.ToggleDrawerAction
+export type T = BoardModule.ClickSquareAction | DrawerModule.ToggleDrawerAction
