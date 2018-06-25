2e3ac43b26a494625b9d7a261914339f67107f9e
namachan10777 <namachan10777@gmail.com>
2018-06-25 18:50:28 +0900

    fix node build error

--- gui/webpack-server.config.js
+++ gui/webpack-server.config.js
@@ -2,9 +2,12 @@ const Base = require('./webpack-base.config.js');
 const WebpackMerge = require('webpack-merge');
 const Path = require('path');
 
+const nodeExternals = require('webpack-node-externals');
+
 module.exports = WebpackMerge(Base.config, {
 	entry: './src/server/main.ts',
 	target: 'node',
+	externals: [nodeExternals()],
 	output: {
 		filename: 'server.js',
 		path: Path.resolve('server')
