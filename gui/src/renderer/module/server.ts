0b9cc65d901e2879986c511b2624d6b377a1ab6a
namachan10777 <namachan10777@gmail.com>
2018-06-27 18:42:56 +0900

    add UPDATE_SOCKET action

--- gui/src/renderer/module/server.ts
+++ gui/src/renderer/module/server.ts
@@ -1,10 +1,12 @@
 import * as Redux from 'redux';
 import * as Action from '../actions';
 import * as Store from '../store';
+import * as IO from 'socket.io-client';
 
 export enum ActionNames {
 	CHANGE_IP_ADDRESS = 'IGOKABADDI_CHANGE_IP_ADDRESS',
-	CHANGE_PORT = 'IGOKABADDI_CHANGE_PORT'
+	CHANGE_PORT = 'IGOKABADDI_CHANGE_PORT',
+	UPDATE_SOCKET = 'IGOKABADDI_UPDATE_SOCKET'
 }
 
 export type ChangeIpAddressAction = {
@@ -21,6 +23,13 @@ export type ChangePortAction = {
 	}
 }
 
+export type UpdateSocketAction = {
+	type: ActionNames.UPDATE_SOCKET;
+	payload: {
+		socket: IO.Socket;
+	}
+}
+
 export function reducer(state: {ip: string; port: string} = Store.initialState.server, action: Action.T) {
 	switch (action.type) {
 	case ActionNames.CHANGE_IP_ADDRESS:
@@ -32,6 +41,11 @@ export function reducer(state: {ip: string; port: string} = Store.initialState.s
 		return {
 			...state,
 			port: action.payload.port
+		};
+	case ActionNames.UPDATE_SOCKET:
+		return {
+			...state,
+			socket: action.payload.socket
 		};
 	default:
 		return state;
