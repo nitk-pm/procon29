fec3a67bfb7a61d8c5d5af6d730d0f488e68ce52
Mackyson <Mackyson@gmail.com>
2018-07-15 02:59:24 +0900

    多分得点計算できた

--- solver/source/calc.d
+++ solver/source/calc.d
@@ -115,6 +115,7 @@ enum ExampleJson = q{
 };
 /*
 	赤チームの領域ポイントを考えるときは青白を一緒と考えてよい、逆もしかり
+	完結に説明するために以下では赤視点でコメントを書いている
 */
 int abs(int a){
 	return a>0?a:-a;
@@ -124,26 +125,26 @@ auto surroundCalc(Square[] b,int width){
 	int[2] score; score[0]=0;score[1]=0;//公式ページに従ってもタプル使えなくてキレそう
 	string[2] team;team[0]="Red";team[1]="Blue"; //このへんめっちゃクソコードだけど多分これが一番速い
 	foreach(color;team){//赤と青を分けて考える
-		int surroundPoint = 0;
 		bool[] visited;
 		foreach(x;b)
 			visited~=false;
 		auto q = Queue!int();
 		for (int i=0;i<b.length;i++){
-			if (b[i].color != "Out" && b[i].color != color){//番兵と今見てるチームのマスは展開しちゃダメ
+			if (b[i].color != "Out" && b[i].color != color && !visited[i]){//番兵と赤のマスと展開済みのマスは展開しちゃダメ
 				q.push(i);
 				visited[i]=true;//最初から番兵を探索済みにすると点数計算がおかしくなる
 			}
+			else continue;
+			auto isSurrounded = true;//囲まれ判定、連結した一つの青白領域に対して考えるのでここで宣言
+			int surroundPoint = 0;
 			while (!q.empty){
-				writeln(q.top());
 				auto idx = q.top(); //現在のインデックス
 				q.pop();
 				surroundPoint += abs(b[idx].score);
-				auto isSurrounded = true;//囲まれ判定
 				int expandIdx; //展開先のインデックス
 				for(int j=0;j<4;++j){
 					final switch(j){
-						//defalut :assert(false); //普通のswitchだとこれ書いててもdefaultないやんけって怒られるのでやむなく。
+						//defalut :assert(false); //普通のswitchだとこれ書いててもdefaultないやんけって怒られるのでやむなくfinal switch。
 						case 0:expandIdx = idx-width;	break;//↑
 						case 1:expandIdx = idx+1;	break;//→
 						case 2:expandIdx = idx+width;	break;//↓
@@ -164,18 +165,35 @@ auto surroundCalc(Square[] b,int width){
 					}
 				}
 			}
+			if (!isSurrounded)
+				surroundPoint = 0;
+			if (color=="Red")
+				score[0]+=surroundPoint;
+			else 
+				score[1]+=surroundPoint;
 		}
-		if (color=="Red")
-			score[0]+=surroundPoint;
-		else 
-			score[1]+=surroundPoint;
 	}
+	return score;
+}
+auto tileCalc(Square[] b){
+	int[2] score;score[0]=0;score[1]=0;
+	foreach(x;b){
+	if (x.color == "Red")	score[0]+=x.score;
+	if (x.color == "Blue")	score[1]+=x.score;
+	}
+	return score;
+}
+auto calc(Square[] b,int width){
+	auto s = surroundCalc(b,width);
+	auto t = tileCalc(b);
+	int [2] score;
+	score[0]=s[0]+t[0];score[1]=s[1]+t[1];
 	return score;
 }
 unittest{
 	auto json = parseJSON(ExampleJson);
 	auto width = width(json);
 	auto board = decode(json);
-	auto r = surroundCalc(board,width);
-	writeln(r[0],":",r[1]);
+	auto r = calc(board,width);
+	writeln("Red:",r[0]," Blue:",r[1]);
 }
