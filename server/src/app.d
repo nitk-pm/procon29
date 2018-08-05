298b4dadf96934b2fac3990d52bb62d89c6dd8ab
namachan10777 <namachan10777@gmail.com>
2018-08-05 22:23:45 +0900

    server: splite files

--- server/src/app.d
+++ server/src/app.d
@@ -1,3 +1,5 @@
+module procon29.server.app;
+
 import std.stdio;
 import std.file;
 import std.json;
@@ -9,87 +11,52 @@ import vibe.http.router;
 import vibe.http.websockets;
 import vibe.d;
 
-enum Color {
-	Red,
-	Blue,
-	Neut
-}
+import procon29.server.board;
 
-struct Square {
-	Color color;
-	int score;
-	bool agent;
-}
+Board board;
+// operation購読者のsocketのリスト
+WebSocket[] opSubscribers;
+// 接続してるSokcet
+WebSocket[] sockets;
 
-alias Board = Square[][];
+Operation[] blueOp, redOp;
+bool blueOpPushed, redOpPushed;
 
+enum LocalHost = ["::1", "127.0.0.1"];
+enum LabAddress = ["192.168.42.151"];
 
-Board boardOfJson(JSONValue json) {
-	Square[][] tbl;
-	foreach(colJson; json.array) {
-		Square[] col;
-		foreach(squareJson; colJson.array) {
-			Color color;
-			switch (squareJson["color"].str) {
-			case "Red": color = Color.Red; break;
-			case "Blue": color = Color.Blue; break;
-			case "Neut": color = Color.Neut; break;
-			default:
-				throw new Exception("Illegal board.json");
-			}
-			bool agent = squareJson["agent"].type == JSON_TYPE.TRUE;
-			int score = squareJson["score"].integer.to!int;
-			col ~= Square(color, score, agent);
-		}
-		tbl ~= col;
+size_t cnt = 0;
+
+StopWatch timekeeper;
+
+shared static this () {
+	auto boardJson = "./board.json".readText.parseJSON;
+	board = boardOfJson(boardJson);
+
+	auto router = new URLRouter;
+	router.get("/", handleWebSockets(&handleConn));
+
+	auto settings = new HTTPServerSettings;
+	settings.port = 8080;
+	settings.bindAddresses = LocalHost ~ LabAddress;
+	timekeeper.start;
+	listenHTTP(settings, router);
+	if (!exists("./log")) {
+		mkdir("./log");
 	}
-	return tbl;
 }
 
-JSONValue jsonOfBoard(Board board) {
-	JSONValue[] boardJson;
-	foreach (col; board) {
-		JSONValue[] colJson;
-		foreach (square; col) {
-			JSONValue squareJson;
-			final switch (square.color) {
-			case Color.Red: squareJson["color"] = JSONValue("Red"); break;
-			case Color.Blue: squareJson["color"] = JSONValue("Blue"); break;
-			case Color.Neut: squareJson["color"] = JSONValue("Neut"); break;
-			}
-			squareJson["score"] = JSONValue(square.score);
-			squareJson["agent"] = JSONValue(square.agent);
-			colJson ~= squareJson;
-		}
-		boardJson ~= JSONValue(colJson);
-	}
-	return JSONValue(boardJson);
+string genReplyMsg(string type, JSONValue json) {
+	JSONValue res;
+	res["type"] = JSONValue(type);
+	res["payload"] = json;
+	return res.toString;
 }
 
-struct Pos {
-	int x, y;
-}
-unittest {
-	assert(Pos(1, 2) == Pos(1, 2));
-}
-
-enum OpType {
-	Move,
-	Clear
-}
-
-struct Operation {
-	OpType type;
-	// fromはtype == OpType.Moveのときのみ有効
-	// 無効な場合は-1, -1を代入しておく
-	Pos from, to;
-}
-
-struct OpContainer {
-	Color color;
-	OpType type;
-	Pos from;
-	Pos to;
+string genTimeReplyMsg() {
+	JSONValue json;
+	json["time"] = timekeeper.peek.total!"msecs".to!float / 1000.0f;
+	return genReplyMsg("distribute-time", json);
 }
 
 Board updateBoard(Board board, Operation[] blueOp, Operation[] redOp) {
@@ -169,78 +136,6 @@ Board updateBoard(Board board, Operation[] blueOp, Operation[] redOp) {
 	return board;
 }
 
-
-Operation[] operationsOfJson(JSONValue json) {
-	Operation[] ops;
-	foreach (op; json.array) {
-		OpType type;
-		Pos from = Pos(-1, -1);
-		Pos to;
-		if (op["type"].str == "Move")
-			type = OpType.Move;
-		else
-			type = OpType.Clear;
-		to.x = op["to"]["x"].integer.to!int;
-		to.y = op["to"]["y"].integer.to!int;
-		if (type == OpType.Move) {
-			from.x = op["from"]["x"].integer.to!int;
-			from.y = op["from"]["y"].integer.to!int;
-		}
-		ops ~= Operation(type, from, to);
-	}
-	return ops;
-}
-
-JSONValue jsonOfOperations(Operation[] operations) {
-	//TODO 実装しろ
-	return JSONValue(0);
-}
-
-Board board;
-// operation購読者のsocketのリスト
-WebSocket[] opSubscribers;
-// 接続してるSokcet
-WebSocket[] sockets;
-
-Operation[] blueOp, redOp;
-bool blueOpPushed, redOpPushed;
-
-enum LocalHost = ["::1", "127.0.0.1"];
-enum LabAddress = ["192.168.42.151"];
-
-size_t cnt = 0;
-
-StopWatch timekeeper;
-
-shared static this () {
-	auto boardJson = "./board.json".readText.parseJSON;
-	board = boardOfJson(boardJson);
-
-	auto router = new URLRouter;
-	router.get("/", handleWebSockets(&handleConn));
-
-	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
-	settings.bindAddresses = LocalHost ~ LabAddress;
-	timekeeper.start;
-	listenHTTP(settings, router);
-	if (!exists("./log")) {
-		mkdir("./log");
-	}
-}
-
-string genReplyMsg(string type, JSONValue json) {
-	JSONValue res;
-	res["type"] = JSONValue(type);
-	res["payload"] = json;
-	return res.toString;
-}
-
-string genTimeReplyMsg() {
-	JSONValue json;
-	json["time"] = timekeeper.peek.total!"msecs".to!float / 1000.0f;
-	return genReplyMsg("distribute-time", json);
-}
 
 // red, blue共にoperationが揃ったら盤面を更新して配信
 // 片方だけしか来て無ければOperationの購読者だけに配信
