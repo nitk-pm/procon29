e927a6decc5cf37a5ac9801a75e70419add9acd6
Mackyson <mackyson72@gmail.com>
2018-09-18 17:17:08 +0900

    通信できるけどセグフォでた

--- solver/source/connecter.d
+++ solver/source/connecter.d
@@ -1,4 +1,6 @@
 module procon.connecter;
+
+import std.stdio;
 import vibe.d;
 import vibe.http.websockets;
 import std.json;
@@ -8,13 +10,14 @@ import procon.encoder;
 import procon.decoder;
 const serverURL = "ws://127.0.0.1:8080";
 
-void connectFlow (int color,int turn){
+void connect (int color,int turn){
 	while (turn>0){
 		auto ws = connectWebSocket(URL.parse(serverURL));
 		while(ws.waitForData()){
 			auto txt = ws.receiveText();
+			writeln(txt);
 			auto json = parseJSON(txt);
-			Board board=decode(json);
+			Board board=decode(json["payload"]);
 			auto opJson = search(color,turn,board);--turn;
 			ws.send(opJson[0].toString);
 			ws.send(opJson[1].toString);
