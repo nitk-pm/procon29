56df670e41f45e3f0c8e4d0c3c7944c1746b4cb8
Mackyson <mackyson72@gmail.com>
2018-09-24 13:08:09 +0900

    Schema修正に追従

--- solver/source/encoder.d
+++ solver/source/encoder.d
@@ -47,12 +47,11 @@ JSONValue makeOperationJson(Color color,Operation[2] rawOp){
 			default :assert(false);
 		}
 		opJsonParts["type"]=type;
-		if (rawOp[i].type==Type.Move){
-			JSONValue tmp;
-			tmp["x"]=JSONValue(rawOp[i].from.x-1);
-			tmp["y"]=JSONValue(rawOp[i].from.y-1);
-			opJsonParts["from"]=tmp;
-		}
+		JSONValue tmp;
+		tmp["x"]=JSONValue(rawOp[i].from.x-1);
+		tmp["y"]=JSONValue(rawOp[i].from.y-1);
+		opJsonParts["from"]=tmp;
+
 		JSONValue tmp;
 		tmp["x"]=JSONValue(rawOp[i].to.x-1);
 		tmp["y"]=JSONValue(rawOp[i].to.y-1);
